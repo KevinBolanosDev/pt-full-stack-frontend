9a478dd6b2069605003f5cbba4902ba99589c072
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2025-04-02 12:23:45 -0500

    Modificando variables para deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 // Conectado backend
 const instance = axios.create({
     baseURL: import.meta.env.VITE_REACT_APP_API_URL,
+    headers: {
+    'Content-Type': 'application/json'
+    }
 });
 
 export default instance;
